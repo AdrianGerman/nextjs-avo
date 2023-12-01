7d7dc90eb79f726fca364c6afd9b60bf1dca2fd3
AdrianGerman <adrian.german1019@gmail.com>
2023-12-01 14:09:23 -0600

    basic routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>Esta es nuestra pagina de About</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hola mundo desde Hallownest!!</h1>
+    </div>
+  );
+};
+
+export default Home;
