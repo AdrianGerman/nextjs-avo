35065895b6483b8b11ff7fa8c6b9e89f84b7a1a4
AdrianGerman <adrian.german1019@gmail.com>
2023-12-02 19:14:57 -0600

    Linking pages Navbar

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import react from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const About = () => {
   return (
     <div>
+      <Navbar />
       <h2>Esta es nuestra pagina de About</h2>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import react from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hola mundo desde Hallownest!!</h1>
     </div>
   );
