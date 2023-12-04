b2b95b0c2ce1fef157144519360f7c8692de3056
AdrianGerman <adrian.german1019@gmail.com>
2023-12-03 20:30:44 -0600

    creating and consuming our own API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window
+      .fetch("/api/avo")
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+        setProductList(data);
+      });
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <div>Platzi and Next.js!</div>
+      <div>Hola muy buenas desde el fin del mundo y antes de ameri!</div>
+      {productList.map((product) => (
+        <div>{product.name}</div>
+      ))}
     </div>
   );
 };
