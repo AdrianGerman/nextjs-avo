bb7c388c5c9f4801476d25f10192d685a9579d04
AdrianGerman <adrian.german1019@gmail.com>
2023-12-04 09:23:24 -0600

    adds semantic-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import "semantic-ui-css/semantic.min.css";
 import Layout from "@components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
